4b8b43513198a20e13fe4a98686977f9f9b265c1
EpicGrappler <epicgrappler337@gmail.com>
2026-09-03 01:35:30 +0300

    Fixing the params path

--- train_K.py
+++ train_K.py
@@ -85,7 +85,7 @@ MODEL_REGISTERY ={
      'RandomForest' : RandomForestClassifier
 }
 
-with open('/kaggle/input/StudentRisk_PARAMS/params.yaml', 'r') as f:
+with open('/kaggle/input/datasets/mostafaashrafdev/studentrisk-params', 'r') as f:
     models = yaml.safe_load(f)['train']['model']
 
 classifier = 'RandomForest'
